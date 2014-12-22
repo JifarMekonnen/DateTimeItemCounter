425ff7a37836b1c3a1402e820beb290f58f0a474
Jifar <Jifar.Mekonnen@dVentus.com>
2014-12-22 14:56:49 +0300

    12_22_14_thrid_and_well_tested_commit

--- DateTimeItemCounter/src/main/java/DateTimeItemCounterPackage/DateTimeItemCounter/DateTimeItemCounterDataHolder.java
+++ DateTimeItemCounter/src/main/java/DateTimeItemCounterPackage/DateTimeItemCounter/DateTimeItemCounterDataHolder.java
@@ -6,27 +6,31 @@ import org.joda.time.DateTime;
 
 public class DateTimeItemCounterDataHolder {
 	
-    DateTime dateTimeArray[] = new DateTime[1000];
+    DateTime dateTimeArray[] = new DateTime[4000];
 	
     public DateTimeItemCounterDataHolder(){
     	populateDateTimeArray();
     	
-    	System.out.println(dateTimeArray[0]);
-    	System.out.println(dateTimeArray[1]);
-    	System.out.println(dateTimeArray[2]);
-    	System.out.println(dateTimeArray[3]);
-    	System.out.println(dateTimeArray[4]);
     }
 	
+    void populateDateTimeArray(){
+		DateTime datetime= new DateTime(2014,12,9,20,35);
+		for(int i=0;i<4000;i++){
+			
+			dateTimeArray[i]=datetime.plusMinutes(i*20);
+		
+		}
+	}
 	
-	DateTime createDateTime(int i){
+    
+	 DateTime createDateTime(int i){
 		DateTime datetime= new DateTime(2014,12,9,20,35);
 		datetime.plusMinutes(i*20);
 		DateTime datetimereturn= datetime;
 		return datetimereturn;
 	}
 	
-	int getRandomBetweenMinAndMax(int min, int max){
+	 int getRandomBetweenMinAndMax(int min, int max){
 
 	    
 	    Random rand = new Random();
@@ -36,16 +40,6 @@ public class DateTimeItemCounterDataHolder {
 	    int randomNum = rand.nextInt((max - min) + 1) + min;
 
 	    return randomNum;
-	}
-	
-	void populateDateTimeArray(){
-		
-		for(int i=0;i<1000;i++){
-			
-			DateTime tempo=createDateTime(i);
-			dateTimeArray[i]=tempo;
-		
-		}
 	}
 
 }
--- DateTimeItemCounter/src/test/java/DateTimeItemCounterPackage/DateTimeItemCounter/DateTimeItemCounterTest.java
+++ DateTimeItemCounter/src/test/java/DateTimeItemCounterPackage/DateTimeItemCounter/DateTimeItemCounterTest.java
@@ -1,54 +1,11 @@
 package DateTimeItemCounterPackage.DateTimeItemCounter;
 
 import junit.framework.TestCase;
-import org.joda.time.DateTime;
+
 
 
 public class DateTimeItemCounterTest extends TestCase {
 
-  
-	
-   
-	
-	
-	
-	
-	
-	
-	
-    DateTime d0= new DateTime(2014,12,9,20,35);
-    
-  
-    DateTime datetime= new DateTime();
-    
-    
-    DateTime d1= new DateTime(2014,12,9,21,00);
-    DateTime d2= new DateTime(2014,12,9,21,20);
-    DateTime d3= new DateTime(2014,12,9,21,43);
-    DateTime d4= new DateTime(2014,12,9,22,03);
-    DateTime d5= new DateTime(2014,12,9,22,24);
-    DateTime d6= new DateTime(2014,12,9,22,45);
-    DateTime d7= new DateTime(2014,12,9,23,05);
-    DateTime d8= new DateTime(2014,12,9,23,27);
-    DateTime d9= new DateTime(2014,12,9,23,50);
-    DateTime d10= new DateTime(2014,12,10,0,11);
-   
-    DateTime d11= new DateTime(2014,12,10,0,35);
-    DateTime d12= new DateTime(2014,12,10,1,00);
-    DateTime d13= new DateTime(2014,12,10,1,20);
-    DateTime d14= new DateTime(2014,12,10,1,43);
-    DateTime d15= new DateTime(2014,12,10,2,03);
-    DateTime d16= new DateTime(2014,12,10,2,24);
-    DateTime d17= new DateTime(2014,12,10,2,45);
-    DateTime d18= new DateTime(2014,12,10,3,05);
-    DateTime d19= new DateTime(2014,12,10,3,27);
-    DateTime d20= new DateTime(2014,12,10,3,50);
-    DateTime d21= new DateTime(2014,12,10,4,11);
-    
-   
-    
-    DateTime array[]={d0,d1,d2,d3,d4,d5,d6,d7,d8,d9,d10,d11,d12,	d13,	d14	,d15	,d16,	d17	,d18	,d19,	d20,d21};
-    
     
     DateTimeItemCounterDataHolder dataHolder=new DateTimeItemCounterDataHolder();
     
@@ -56,34 +13,34 @@ public class DateTimeItemCounterTest extends TestCase {
 	
 	public void testcompare_time()
 	    {
-		 int result= dt_count.compare_time(dataHolder.dateTimeArray[0],dataHolder.dateTimeArray[4]);
-	     assertEquals(0,result );
+		 int result= dt_count.compare_time(dataHolder.dateTimeArray[99],dataHolder.dateTimeArray[990]);
+	     assertEquals(1,result );
 	    }
 	
-	public void testgetTimeDifferenceInMinutes()
+    public void testgetTimeDifferenceInMinutes()
     {
-	 long result= dt_count.getTimeDifferenceInMinutes(d10,d11);
-     assertEquals(24,result);
+	 long result= dt_count.getTimeDifferenceInMinutes(dataHolder.dateTimeArray[100],dataHolder.dateTimeArray[104]);
+     assertEquals(80,result);
     }
 	
 	public void testgetIndex()
     {
-	 int result= dt_count.getIndex(array,d13);
-     assertEquals(13,result );
+	 int result= dt_count.getIndex(dataHolder.dateTimeArray,dataHolder.dateTimeArray[116]);
+     assertEquals(116,result );
     }
 	
 	public void testbinarySearch()
     {
-	 int result= dt_count.binarySearch(array,d13);
-     assertEquals(13,result );
+	 int result= dt_count.binarySearch(dataHolder.dateTimeArray,dataHolder.dateTimeArray[116]);
+     assertEquals(116,result );
     }
 	
 	public void testgetNumberOfItems()
     {
-	 int[] result= dt_count.getNumberOfItems(array, d1, d5);
+	 int[] result= dt_count.getNumberOfItems(dataHolder.dateTimeArray,dataHolder.dateTimeArray[3000],dataHolder.dateTimeArray[3999]);
 	 
-     assertEquals("No of Items between the two DateTime items should be 3 ",3,result[1]);
-     assertEquals("The index of the first DateTime item in the array should be 0 ",1,result[0]);
+     assertEquals("No of Items between the two DateTime items should be ",998,result[1]);
+     assertEquals("The index of the first DateTime item in the array should  0 ",3000,result[0]);
      
     }
   
